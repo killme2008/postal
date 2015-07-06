749a1ed4d6f1171754ca3500d5d18f7a3628b552
dennis zhuang <killme2008@gmail.com>
2015-07-06 13:49:43 +0800

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cn.leancloud/postal "1.12-RC1"
+(defproject com.avos/postal "1.12-RC1"
   :description "JavaMail on Clojure"
   :url "https://github.com/drewr/postal"
   :license {:name "MIT"
